40ea2c5394c86f18b429abdc46d7ee27f701bcf6
Benjamin Zaporzan <benzaporzan@gmail.com>
2020-01-08 20:21:56 -0500

    First Commit

---
+++ src/edna/core.clj
@@ -0,0 +1,6 @@
+(ns edna.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/edna/core_test.clj
@@ -0,0 +1,7 @@
+(ns edna.core-test
+  (:require [clojure.test :refer :all]
+            [edna.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
